82a125a1aec770f5a91a5fe53ee737d4c1f109fa
neil <neil.wang@te.com>
2016-02-29 17:33:13 +0800

    Have the restful api setup and work with serialport.

--- WebSerial.js
+++ WebSerial.js
@@ -1,14 +1,57 @@
 var http    = require('http')
 var express = require('express');
+var app     = express();
+var morgan  = require('morgan');
 
-http.createServer(function(requrest, response){
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
-
-console.log('Server Running');
+var bodyParser = require('body-parser');
 
 
+// configure app
+app.use(morgan('dev'));     // Log requests to console
+
+// configure body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// Configure database (using mongodb)
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/neiltestdb');
+var Command = require('./models/Command');
+
+
+// ROUTES FOR OUR API
+// ======================================================================
+
+// create the router
+var router = express.Router();
+
+router.use(function(req, res, next){
+    console.log('A request is coming');
+    next();
+});
+
+router.get('/', function(req, res){
+    res.json({message: 'Welcome to api test'});
+});
+
+router.route('/commands')
+    .post(function(req, res){
+        serialPort.write(req.body.command);
+
+    })
+    .get(function(req, res){
+       Command.find(function(err, commands){
+           if(err)
+               res.send(err);
+           res.json(commands);
+       })
+    });
+
+
+
+// Serial port functions.
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyUSB0", {
     baudrate: 57600
@@ -18,9 +61,20 @@ serialPort.open(function (error) {
     if ( error ) {
         console.log('failed to open: '+error);
     } else {
-        console.log('open');
+        console.log('Serial Port open');
         serialPort.on('data', function(data) {
+
             console.log('data received: ' + data);
+            var command = new Command();
+            command.command = data;
+            var date = new Date();
+            command.time = date.getHours();
+            command.save(function(err){
+                if(err)
+                    console.log(err);
+                console.log('Command Reeived');
+            })
+
         });
 
         serialPort.write("Hello World!\n", function(err, results) {
@@ -31,3 +85,11 @@ serialPort.open(function (error) {
 });
 
 console.log('Serial Port Running');
+
+
+// REGISTER OUR ROUTES ---------------------------------------------
+app.use('/api', router);
+
+// START THE Server ------------------------------------------------
+app.listen(port);
+console.log('Server Start');
